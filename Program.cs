378d7c03b166d84371b08274c9813339aca3c71c
marcelopbg <mpbguima@gmail.com>
2021-06-04 13:37:57 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml;
+using System.Xml.Schema;
+
+namespace DTDValidator
+{
+    class Program
+    {
+        private static bool DocumentHasErrors = false;
+
+        private static List<string> messageErrors = new List<string>();
+        public static void Main()
+        {
+            try
+            {
+                XmlReaderSettings settings = new XmlReaderSettings();
+                settings.DtdProcessing = DtdProcessing.Parse;
+                settings.XmlResolver = new XmlUrlResolver();
+                settings.ValidationType = ValidationType.DTD;
+                settings.ValidationEventHandler += new ValidationEventHandler(ValidationCallBack);
+                XmlReader reader = XmlReader.Create("./assets/vw.xml", settings);
+
+                while (reader.Read()) ;
+            }
+            catch (Exception e)
+            {
+                messageErrors.Add(e.Message);
+                DocumentHasErrors = true;
+            }
+            finally
+            {
+                if (DocumentHasErrors)
+                {
+                    Console.WriteLine("XML Document is Badly Formatted");
+                    Console.WriteLine("Errors: \n");
+                    foreach (var item in messageErrors)
+                    {
+                        Console.WriteLine($"{item} \n");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("XML Document is Well Formatted");
+                }
+                Console.ReadKey();
+            }
+        }
+
+        private static void ValidationCallBack(object sender, ValidationEventArgs e)
+        {
+            messageErrors.Add(e.Message);
+            DocumentHasErrors = true;
+        }
+    }
+}
